82a8ed93b48e4dad52f250320b440f80654c71d9
Kyle Scott <kyscott@umich.edu>
2024-08-13 20:50:32 -0400

    fix

--- packages/core/src/sync/local.ts
+++ packages/core/src/sync/local.ts
@@ -128,6 +128,9 @@ export const createLocalSync = async (
     get finalizedBlock() {
       return finalizedBlock;
     },
+    set finalizedBlock(block) {
+      finalizedBlock = block;
+    },
     async sync() {
       /**
        * Select a range of blocks to sync bounded by `finalizedBlock`.
